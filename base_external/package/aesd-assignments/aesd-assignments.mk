b1be3e5ec935d6330a435e1e50f195d6b1df2abc
pankaj4007 <pankajdineshsolanki@gmail.com>
2021-04-18 23:30:03 -0600

    Message queue testing

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = 'd5937b72e536c7ad060024a5c62b806719b439bd'
+AESD_ASSIGNMENTS_VERSION = '03a0901c0b33724ea9c1826bb47fff5b50ae95a6'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-5013/final-project-adityavarshney100.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
@@ -20,6 +20,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/client_test $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/server_test $(TARGET_DIR)/bin
 	$(INSTALL) -m 0755 $(@D)/uart_test $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/reader_test $(TARGET_DIR)/bin
+	$(INSTALL) -m 0755 $(@D)/writer_test $(TARGET_DIR)/bin
 
 endef
 
